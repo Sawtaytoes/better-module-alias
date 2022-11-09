351bbe96fd2a9ebcf01798a3eb104c50ca450450
Lawlzer <Kevindaspam@gmail.com>
2022-11-08 21:05:52 -0600

    Update Tests to reflect new feature

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-require('./setupModuleAliases')(__dirname,
-	// {"$dirTest": "./dirTest"}, // To test the secondary method, edit package.json and uncomment this
-	)
+require('./setupModuleAliases')(__dirname)
 
+
+// This test is in ./tests/dirTest1
+// By only requiring $test1, we are sure the package is correctly updating the path.
 const assert = require('assert')
-const text = require('$dirTest')
+const text1 = require('$test1')
+assert.equal(text1, 'It works - 1', "Directory aliasing isn't functioning correctly.")
 
-assert
-.equal(
-	text,
-	'It works!',
-	"Directory aliasing isn't functioning correctly.",
-)
+// This test is in ./tests/dirTest1
+// By only requiring $test2, we are sure the package is correctly updating the path.
+require('./setupModuleAliases')(__dirname, {"$test2": "./tests/dirTest2"})
+const text2 = require('$test2')
+assert.equal(text2, 'It works - 2', "Directory aliasing isn't functioning correctly.")
 
-console
-.info('All tests passing!')
+console.info('All tests passing!')
+		
